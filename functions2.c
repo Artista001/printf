84dd6cf2099e18f557b15ab92b938535f6d3ddc8
KipKirwa <117752803+Kipkirwa@users.noreply.github.com>
2023-01-29 22:02:37 +0300

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -10,7 +10,7 @@
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed.
- */
+ **/
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -62,7 +62,7 @@ int print_pointer(va_list types, char buffer[],
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
- */
+ **/
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
